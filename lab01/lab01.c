3b5bf5d614446664124737b16e6622f836467bbd
Tiago Feliciano Gomes <tfelicianogomes@gmail.com>
2019-08-25 14:49:32 -0300

    Corrigindo lugar do arquivo

--- lab01/lab01.c
+++ lab01/lab01.c
@@ -1,59 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printarMatriz(float matriz[100][100], int m, int n){
-    int i, j;
-
-    for(i = 0; i < m; i ++) {
-        for(j = 0; j < n; j++) {
-            printf("%.1f ", matriz[i][j]);
-        }
-        printf("\n");
-    }
-}
-
 void gerarMatrizCaracol(float vetor[100], int m, int n){
     float matriz[m][n];
     int i = m-1, j = n-1, direita = n-1, esquerda = 0, topo = 0, base = m-1, indiceVetor = 0;
     while (indiceVetor < m * n) {
-        while (i >= topo) {
-            matriz[i--][j] = vetor[indiceVetor];
+        while (i >= topo && indiceVetor < m * n) {
+            matriz[i][j] = vetor[indiceVetor];
+            i--;
             indiceVetor++;
-        }
+        }     
+        i++;
         topo++;
         direita--;
         j--;
-        while (j >= esquerda) {
-            matriz[i][j--] = vetor[indiceVetor];
+        while (j >= esquerda && indiceVetor < m * n) {
+            matriz[i][j] = vetor[indiceVetor];
+	    matriz[i][j] = vetor[indiceVetor];
+            j--;
             indiceVetor++;
         }
+	j++;
         esquerda++;
         i++;
-        while (i <= base) {
-            matriz[i++][j] = vetor[indiceVetor];
+        while (i <= base && indiceVetor < m * n) {
+            matriz[i][j] = vetor[indiceVetor];
+	    matriz[i][j] = vetor[indiceVetor];
+            i++;
             indiceVetor++;
-        }
-        base++;
+        }	
+        i--;
+        base--;
         j++;
-        while (j <= direita) {
-            matriz[i][j++] = vetor[indiceVetor];
+        while (j <= direita && indiceVetor < m * n) {
+            matriz[i][j] = vetor[indiceVetor];
+	    matriz[i][j] = vetor[indiceVetor];
+            j++;
             indiceVetor++;
-        }
-        direita++;
-        i++;
+        }	
+        j--;
+        direita--;
+        i--;
     }
 
-    printarMatriz(matriz, m, n);
+    for(i = 0; i < m; i ++) {
+        for(j = 0; j < n; j++) {
+            printf("%.1f", matriz[i][j]);
+            if (j != n-1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
 
 }
 
 int main() {
     int m, n;
-    scanf("%d", &m);
-    scanf("%d", &n);
+    scanf("%d %d", &m, &n);
 
     float vetor[100];
     int i;
+
+    for (i = 0; i < 100; i++) {
+        vetor[i] = 0;
+    }
 
     for (i = 0; i < m * n; i++) {
         scanf("%f", &vetor[i]);
