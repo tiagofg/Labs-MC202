394f879707ce150fd25fbcec213a7afda2afda6d
Tiago Feliciano Gomes <tiagofg@ciandt.com>
2019-10-25 17:47:16 -0300

    Inicializando lab09

---
+++ lab09/lab09.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "heap.h"
+
+int main()
+{
+   int n, m;
+
+   scanf("%d", &n);
+
+   for (int i = 0; i < n; ++i) {
+
+      scanf("%d", &m);
+
+
+   }
+
+   return EXIT_SUCCESS;
+
+}
