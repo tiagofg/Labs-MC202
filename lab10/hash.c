bbe9f9256503d2c763888e1f0bd3ab51cf7b0986
Tiago Feliciano Gomes <tfelicianogomes@gmail.com>
2019-11-17 19:39:37 -0300

    Lab 10 funcionando

--- lab10/hash.c
+++ lab10/hash.c
@@ -4,41 +4,43 @@
 
 #include "hash.h"
 
-Hash* criar_hash() { //criando hash e inicializando as chaves
+Hash* criar_hash(int n) { //criando hash e inicializando as chaves
     Hash* hash = malloc(sizeof(Hash));
 
-    for (int i = 0; i < MAX; i++) {
+    hash->vetor = malloc(n * sizeof(No));
+
+    for (int i = 0; i < n; i++) {
         strcpy(hash->vetor[i].chave, "");
     }
 
     return hash;
 }
 
-int gerar_hash(char *chave) {
+int gerar_hash(char *chave, int num) {
     int i, n = 0;
 
     //gerando hash para cada chave
     for (i = 0; i < strlen(chave); i++){
-        n = (256 * n + chave[i]) % MAX;
+        n = (256 * n + chave[i]) % num;
     }
 
     return n;
 }
 
-void inserir(Hash* hash, char *chave, int num_ocorrencias) {
+void inserir(Hash* hash, char *chave, int n, int ordem) {
     //inicializando nó
     No no;
     strcpy(no.chave, chave);
-    no.num_ocorrencias = num_ocorrencias;
+    no.ordem = ordem;
 
-    int posicao_para_inserir = gerar_hash(chave); //pegando posição para inserir
+    int posicao_para_inserir = gerar_hash(chave, n); //pegando posição para inserir
     int inseriu = 0; //int para verificar se padrão já foi inserido
 
     if (strcmp(hash->vetor[posicao_para_inserir].chave, "") == 0) {
         //inserir na posição que o hash retornou
         hash->vetor[posicao_para_inserir] = no;
     } else {
-        for (int i = posicao_para_inserir; i < MAX; i++) {
+        for (int i = posicao_para_inserir; i < n; i++) {
             //inserir depois da posição que o hash retornou
             if (strcmp(hash->vetor[i].chave, "") == 0) {
                 hash->vetor[i] = no;
@@ -60,12 +62,12 @@ void inserir(Hash* hash, char *chave, int num_ocorrencias) {
     }
 }
 
-No find_padrao(Hash* hash, char* chave) {
+No find_padrao(Hash* hash, int ordem, int n) {
     No no;
 
     //achando no que contém o padrão dentro do hash
-    for (int i = 0; i < MAX; i++) {
-        if (strcmp(hash->vetor[i].chave, chave) == 0) {
+    for (int i = 0; i < n; i++) {
+        if (hash->vetor[i].ordem == ordem) {
             no = hash->vetor[i];
         }
     }
--- lab10/lab10.c
+++ lab10/lab10.c
@@ -6,14 +6,20 @@
 
 int get_qtde_ocorrencias(char* padrao, char* texto) {
     int qtde_ocorrencias = 0;
-    const char *sub_str = texto;
+    int tamanho_padrao = strlen(padrao);
+    char* sub_str = malloc(tamanho_padrao + 1 * sizeof(char));
 
     //pegando a quantidade de ocorrencias de uma chave em um texto
-    while((sub_str = strstr(sub_str, padrao))) //percorrendo texto até o final
-    {
-       qtde_ocorrencias++;
-       sub_str++; //percorrendo string a partir do último padrão encontrado
+    for (int i = 0; i < strlen(texto); i++) {
+        strncpy(sub_str, texto + i, tamanho_padrao);
+
+        //verificando se a substring é igual o padrão
+        if (strcmp(sub_str, padrao) == 0) {
+            qtde_ocorrencias++;
+        }
     }
+
+    free(sub_str);
 
     return qtde_ocorrencias;
 }
@@ -27,41 +33,34 @@ int main()
     Hash* hash = criar_hash(n);
 
     //strings para ler cada padrão e o texto completo
-    char* padrao = malloc(100 * sizeof(char));
+    char* padrao = malloc(50 * sizeof(char));
     char* texto = malloc(40000 * sizeof(char));
 
-    //vetor de strings para manter a ordem de inserção dos padrões
-    char** padroes = (char**) malloc(n * sizeof(char*));
-    for (int i = 0; i < n; i++) {
-        padroes[i] = (char*) malloc(100 * sizeof(char));
-    }
+    //vetor de inteiros para manter a ordem de inserção dos padrões
+    int* qtde_padroes = malloc(n * sizeof(int));
 
     //adicionando padrão no vetor de strings e no hash
     for (int i = 0; i < n; i++) {
         scanf("%s\n", padrao);
-        strcpy(padroes[i], padrao);
-        inserir(hash, padrao, 0);
+        qtde_padroes[i] = 0;
+        inserir(hash, padrao, n, i);
     }
 
     scanf("%s\n", texto);
 
     for (int i = 0; i < n; i++) {
-        No no = find_padrao(hash, padroes[i]);
-        no.num_ocorrencias = get_qtde_ocorrencias(no.chave, texto);
+        No no = find_padrao(hash, i, n);
+        qtde_padroes[i] = get_qtde_ocorrencias(no.chave, texto);
         //imprimindo número de ocorrências para cada padrão
-        printf("%d\n", no.num_ocorrencias);
+        printf("%d\n", qtde_padroes[i]);
     }
 
     //liberando memória
+    free(hash->vetor);
     free(hash);
     free(padrao);
     free(texto);
-
-    for (int i = 0; i < n; i++) {
-        free(padroes[i]);
-    }
-
-    free(padroes);
+    free(qtde_padroes);
 
    return EXIT_SUCCESS;
 
